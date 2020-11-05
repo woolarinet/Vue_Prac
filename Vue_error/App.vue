50dc04bb3cc5d932fe20ab7fa1c72a094edd354f
woolarinet <66486641+woolarinet@users.noreply.github.com>
2020-11-06 01:42:37 +0900

    에러
    app.js의 길이가 점점늘며, App.vue가 style태그를 인식을 못함

---
+++ Vue_error/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1 class = "message">
+        {{ msg }}
+    </h1>
+</template>
+
+<script>
+    export default{
+        data(){
+            return{
+                msg: 'Hello SUNHO!'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .message{
+        color: red;
+    }
+</style>>
---
+++ Vue_error/webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+require('@babel/polyfill')
+
+module.exports = {
+    //  진입점
+    entry : {
+        app2 : [
+            '@babel/polyfill',
+            path.join(__dirname, 'main.js')
+        ]
+    },
+    // 결과물에 대한 설정
+    output : {
+        filename : '[name].js', // app.js
+        path : path.join(__dirname, 'dist2')
+    },
+    module : {
+        rules: [
+            {
+                test: /\.vue$/,
+                use: 'vue-loader'
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+    plugins : [
+        new VueLoaderPlugin(),
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'index.html')
+        })
+    ]
+}
